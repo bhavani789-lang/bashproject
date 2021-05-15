35fcf126e01b10a439ce3ca9e1200f2a88ff3e67
BHAVANI <bhavanarani797@gmail.com>
2021-05-15 12:03:39 +0530

    adding new changes

---
+++ myproj/src/com/myproj/addition.java
@@ -0,0 +1,13 @@
+package com.myproj;
+
+public class addition {
+public int m1(int a,int b)
+{
+	int c =a+b;
+	return c;
+}
+public static void main(String[] args) {
+	addition ad = new addition();
+	ad.m1(20, 30);
+}
+}
